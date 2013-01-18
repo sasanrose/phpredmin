eaa197871f87b96d69723de9b3cdc1df5d02c6fb
Sasan Rose <sasan.rose@gmail.com>
2013-01-18 23:28:53 +0330

    A flag for showing headers or not

--- libraries/drivers/template/json.php
+++ libraries/drivers/template/json.php
@@ -1,12 +1,9 @@
 <?php
 class JsonTemplate
 {
-    public function render($data)
+    public function render($data, $setHeader = False)
     {
-        if ($data)
-            header('HTTP/1.0 200 OK');
-        else
-            header('HTTP/1.0 404 Not Found');
+        $setHeader AND ($data ? header('HTTP/1.0 200 OK') : header('HTTP/1.0 404 Not Found'));
 
         echo json_encode($data);
         die();
