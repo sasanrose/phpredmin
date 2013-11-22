7a86a322a5474bb031b63a73b54db2933c56fa99
Eugene Fidelin <eugene.fidelin@gmail.com>
2013-11-22 15:32:41 +0100

    Display edit form after submit

--- views/hashes/edit.php
+++ views/hashes/edit.php
@@ -10,16 +10,14 @@
             There was a problem editing the hash key
         </div>
     <?php } ?>
-    <?php if (!isset($this->edited) || (isset($this->edited) && !$this->edited)) { ?>
-        <form class="form" action="<?=$this->router->url?>/hashes/edit/<?= $this->app->current['serverId'] . '/' . $this->app->current['database'] ?>" method="post">
-            <legend>Edit hash key</legend>
-            <h5><?=$this->key?> / <?=$this->member?></h5>
-            <div>
-                <textarea name="newvalue"><?=$this->value?></textarea>
-            </div>
-            <input name="key" value="<?=$this->key?>" type="hidden" />
-            <input name="member" value="<?=$this->member?>" type="hidden" />
-            <button type="submit" class="btn"><i class="icon-edit"></i> Edit</button>
-        </form>
-    <?php } ?>
+    <form class="form" action="<?=$this->router->url?>/hashes/edit/<?= $this->app->current['serverId'] . '/' . $this->app->current['database'] ?>" method="post">
+        <legend>Edit hash key</legend>
+        <h5><?=$this->key?> / <?=$this->member?></h5>
+        <div>
+            <textarea name="newvalue"><?=$this->value?></textarea>
+        </div>
+        <input name="key" value="<?=$this->key?>" type="hidden" />
+        <input name="member" value="<?=$this->member?>" type="hidden" />
+        <button type="submit" class="btn"><i class="icon-edit"></i> Edit</button>
+    </form>
 </div>
