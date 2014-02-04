9971c4949a26bac2430a5bd9e6c2acc1d4ce0496
Ryan Holmes <holmes.ryan.90@gmail.com>
2014-02-04 23:17:51 +0000

    Fixes database names for multi-servers

--- libraries/controller.php
+++ libraries/controller.php
@@ -34,7 +34,9 @@ class Controller
                 $current['dbs'][] = array(
                     'id' => $i,
                     'keys' => $matches[1],
-                    'name' => (isset($this->app->config['database']['names'][$i]) ? $this->app->config['database']['names'][$i] : null),
+                    'name' => ( isset($this->app->config['database']['redis'][$current['serverId']]['dbNames'][$i]) ?
+                            $this->app->config['database']['redis'][$current['serverId']]['dbNames'][$i] :
+                            null ),
                 );
             }
         }
