bf88d181b9eac1975e8fd38ceb189a7451e25850
Ryan Holmes <holmes.ryan.90@gmail.com>
2014-02-05 04:06:28 +0000

    Started work on tab styling. Welcome page looks great, other pages not so much. Need to find a way for other pages to play nicely

--- views/navigation.php
+++ views/navigation.php
@@ -5,34 +5,28 @@
             <i class="icon-chevron-down"></i> <?= $this->app->current['host'] ?>:<?= $this->app->current['port'] ?>
         </a>
     </li>
-    <?php foreach ($this->app->current['dbs'] as $database): ?>
-        <?php 
-            if ($database['id'] == $this->app->current['database']) {
-                $dbClass = 'active';
-                $dbIcon = 'icon-ok-sign';
-            } 
-            else {
-                $dbClass = '';
-                $dbIcon = '';
-            }
-        ?>
-        <li class="database <?= $dbClass ?>">
-            <a href="<?=$this->router->url?>/welcome/index/<?= $this->app->current['serverId'] . '/' . $database['id'] ?>">
-                <i class="<?= $dbIcon ?>"></i> <?= ($database['name'] ? $database['name'] : 'DB ' . $database['id']) ?>
-                <span class="label pull-right" title="Number of keys"><?= $database['keys'] ?></span>
-            </a>
-        </li>
-    <?php endforeach; ?>
-    <?php if (count($this->app->config['database']['redis']) > 1) : ?>
-        <li class="nav-header">Other servers</li>    
-        <?php foreach ($this->app->config['database']['redis'] as $serverId => $server): ?>
-            <?php if ($serverId != $this->app->current['serverId']) : ?>
-                <li class="server">
-                    <a href="<?=$this->router->url?>/welcome/index/<?= $serverId . '/0' ?>">
-                        <i class="icon-chevron-right"></i> <?= $server['host'] ?>:<?= $server['port'] ?>
-                    </a>
-                </li>
-            <?php endif; ?>
-        <?php endforeach; ?>  
-    <?php endif; ?>
+</ul>
+<div class="tabbable tabs-left" id="dbTabs">
+    <ul class="nav nav-tabs">
+        <?php foreach ($this->app->current['dbs'] as $database): ?>
+            <li class="database <?= ($database['id'] == $this->app->current['database'] ? 'active':null) ?>">
+                <a href="<?=$this->router->url?>/welcome/index/<?= $this->app->current['serverId'] . '/' . $database['id'] ?>">
+                    <i class="<?= $dbIcon ?>"></i> <?= ($database['name'] !== null ? $database['name'] : "DB ".$database['id']) ?>
+                    <span class="label pull-right" title="Number of keys"><?= $database['keys'] ?></span>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+<ul class="nav nav-pills nav-stacked" id="srvList">
+    <li class="nav-header">Other servers</li>    
+    <?php foreach ($this->app->config['database']['redis'] as $serverId => $server): ?>
+        <?php if ($serverId != $this->app->current['serverId']) : ?>
+            <li class="server">
+                <a href="<?=$this->router->url?>/welcome/index/<?= $serverId . '/0' ?>">
+                    <i class="icon-chevron-right"></i> <?= $server['host'] ?>:<?= $server['port'] ?>
+                </a>
+            </li>
+        <?php endif; ?>
+    <?php endforeach; ?>  
 </ul>                                
