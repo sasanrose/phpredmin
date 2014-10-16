edcb1fa11150bad0759eab1eabbfaadb681fc713
Luke Anderson <stuntguy3000@gmail.com>
2014-10-16 16:50:56 +1030

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -73,7 +73,7 @@
                                 <?php endif; ?>
                                 <li>
                                     <a href="https://github.com/sasanrose/phpredmin" target="_blank">
-                                        <i class="icon-white icon-github"></i> Github
+                                        <i class="icon-white icon-github"></i> GitHub
                                     </a>
                                 </li>
                             </ul>
