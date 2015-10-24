885eaa2fc73219faf1ec7aeca2cc571e2e4d969e
Luong Vu <luongvu@MBAir.local>
2015-10-24 14:04:26 +0700

    add basic auth with configurable username and password

--- public/index.php
+++ public/index.php
@@ -1,28 +1,56 @@
 <?php
 
+
 function __autoload($class)
 {
-    $path = '../';
-    if (preg_match('/^(.*)_Controller$/', $class, $matches)) {
-        $class = $matches[1];
-        $dir   = 'controllers';
-    } elseif (preg_match('/^(.*)_Model$/', $class, $matches)) {
-        $class = $matches[1];
-        $dir   = 'models';
-    } elseif (preg_match('/^(.*)_Helper$/', $class, $matches)) {
-        $class = $matches[1];
-        $dir   = 'helpers';
-    } else {
-        $dir = 'libraries';
-    }
+  $path = '../';
+  if (preg_match('/^(.*)_Controller$/', $class, $matches)) {
+    $class = $matches[1];
+    $dir   = 'controllers';
+  } elseif (preg_match('/^(.*)_Model$/', $class, $matches)) {
+    $class = $matches[1];
+    $dir   = 'models';
+  } elseif (preg_match('/^(.*)_Helper$/', $class, $matches)) {
+    $class = $matches[1];
+    $dir   = 'helpers';
+  } else {
+    $dir = 'libraries';
+  }
 
-    include_once($path.$dir.'/'.(strtolower($class)).'.php');
+  include_once($path.$dir.'/'.(strtolower($class)).'.php');
 }
 
 if (isset(App::instance()->config['timezone'])) {
-    date_default_timezone_set(App::instance()->config['timezone']);
+  date_default_timezone_set(App::instance()->config['timezone']);
 }
 
-$error = new Error();
+$username = null;
+$password = null;
+$auth = array(
+  'username'=>App::instance()->config['auth']['username'],
+  'password'=>App::instance()->config['auth']['password']
+);
+// mod_php
+if (isset($_SERVER['PHP_AUTH_USER'])) {
+  $username = $_SERVER['PHP_AUTH_USER'];
+  $password = $_SERVER['PHP_AUTH_PW'];
+  // most other servers
+} elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+  if (strpos(strtolower($_SERVER['HTTP_AUTHORIZATION']),'basic')===0)
+  list($username,$password) = explode(':',base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
 
-Router::instance()->route();
+}
+//modify username and password to suit your need
+if($username ==$auth['username'] && $password == $auth['password'])
+{
+  $error = new Error();
+  Router::instance()->route();
+}
+else {
+  header('WWW-Authenticate: Basic realm="PHPRedis Administrator"');
+  header('HTTP/1.0 401 Unauthorized');
+  echo 'Not Authorized';
+  die();
+
+
+}
