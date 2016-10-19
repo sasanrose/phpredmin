e5d14d15aeb262d5880535332ff9e5c1c4e84c67
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2016-10-19 18:50:24 +0200

    Added coherent header comment to every file

--- controllers/lists.php
+++ controllers/lists.php
@@ -1,5 +1,24 @@
 <?php
 
+/*
+ * This file is part of the "PHP Redis Admin" package.
+ *
+ * (c) Faktiva (http://faktiva.com)
+ *
+ * NOTICE OF LICENSE
+ * This source file is subject to the CC BY-SA 4.0 license that is
+ * available at the URL https://creativecommons.org/licenses/by-sa/4.0/
+ *
+ * DISCLAIMER
+ * This code is provided as is without any warranty.
+ * No promise of being safe or secure
+ *
+ * @author   Sasan Rose <sasan.rose@gmail.com>
+ * @author   Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
+ * @license  https://creativecommons.org/licenses/by-sa/4.0/  CC-BY-SA-4.0
+ * @source   https://github.com/faktiva/php-redis-admin
+ */
+
 class Lists_Controller extends Controller
 {
     public function addAction()
