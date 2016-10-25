f612661af75aae7b19de39cc9f250c776873f291
Sasan Rose <sasan.rose@gmail.com>
2016-10-26 07:52:48 +1100

    The ability to override configs by ENV vars

--- libraries/app.php
+++ libraries/app.php
@@ -8,7 +8,9 @@ final class app
     protected function __construct()
     {
         $this->_data['config']  = include_once(file_exists('../config.php') ? '../config.php' : '../config.dist.php');
-        $this->_data['drivers'] = 'drivers/';
+		$this->_data['drivers'] = 'drivers/';
+
+		$this->readEnvConfig();
     }
 
     public static function instance()
@@ -28,5 +30,38 @@ final class app
     public function __set($key, $value)
     {
         $this->_data[$key] = $value;
-    }
+	}
+
+	protected function readEnvConfig() {
+		$envConf = preg_grep('/^PHPREDMIN_/', array_keys($_ENV));
+
+		if (!empty($envConf)) {
+			foreach ($envConf as $conf) {
+				$keys = explode('_', $conf);
+
+				if (!empty($keys)) {
+					array_shift($keys);
+
+					self::setConfig($this->_data['config'], $keys, $_ENV[$conf]);
+				}
+			}
+		}
+	}
+
+	protected static function setConfig(&$config, $keys, $value) {
+		$key = array_shift($keys);
+
+		$key = strtolower($key);
+
+		if (isset($config[$key])) {
+			if (is_array($config[$key])) {
+				return self::setConfig($config[$key], $keys, $value);
+			} else {
+				$config[$key] = $value;
+				return True;
+			}
+		}
+
+		return False;
+	}
 }
