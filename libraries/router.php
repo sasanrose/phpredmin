d78f38d7c8dc501752f03e2bb607f023bca0f7b9
Markus Perl <markus@www-factory.de>
2017-01-24 11:26:31 +0100

    Undefined index REQUEST_METHOD and HTTP_HOST while running the cron job

--- libraries/router.php
+++ libraries/router.php
@@ -28,12 +28,19 @@ final class router
 
     protected function parse()
     {
-        $this->method   = $_SERVER['REQUEST_METHOD'];
+
+        if (PHP_SAPI != 'cli') {
+            $this->method = $_SERVER['REQUEST_METHOD'];
+            $this->host = $_SERVER['HTTP_HOST'];
+        } else {
+            $this->method = self::GET;
+            $this->host = null;
+        }
+
         $this->protocol = (
             (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && $_SERVER['HTTPS'] !== 'off')
             || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
         ) ? 'https' : 'http';
-        $this->host     = $_SERVER['HTTP_HOST'];
         $this->baseUrl  = $this->protocol.'://'.$this->host;
         $this->url      = $this->protocol.'://'.$this->host.$_SERVER['SCRIPT_NAME'];
         $this->path     = '';
