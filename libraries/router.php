61b17332dfd96825db1535437f48068bf21003e8
Sasan Rose <sasan.rose@gmail.com>
2017-02-17 19:54:36 +1100

    We do not need protocol anymore

--- libraries/router.php
+++ libraries/router.php
@@ -29,10 +29,6 @@ final class router
     protected function parse()
     {
         $this->method   = $_SERVER['REQUEST_METHOD'];
-        $this->protocol = (
-            (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && $_SERVER['HTTPS'] !== 'off')
-            || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
-        ) ? 'https' : 'http';
         $this->host     = $_SERVER['HTTP_HOST'];
         $this->baseUrl  = '//'.$this->host;
         $this->url      = '//'.$this->host.$_SERVER['SCRIPT_NAME'];
